3d84aeb6ffd4d0ae3fe9d44cb7121e1d50961911
Konrad Kęska <keska.konrad@gmail.com>
2024-02-03 20:11:33 +0100

    init

---
+++ src/site.config.ts
@@ -0,0 +1,77 @@
+import type { SiteConfig } from "@/types";
+import { ExpressiveCodeTheme, type AstroExpressiveCodeOptions } from "astro-expressive-code";
+import fs from "node:fs";
+
+export const siteConfig: SiteConfig = {
+	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+	author: "Konrad Keska",
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	title: "blog",
+	// Meta property used as the default description meta property
+	description: "An opinionated starter theme for Astro",
+	// HTML lang property, found in src/layouts/Base.astro L:18
+	lang: "en-GB",
+	// Meta property, found in src/components/BaseHead.astro L:42
+	ogLocale: "en_GB",
+	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+	date: {
+		locale: "en-GB",
+		options: {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+		},
+	},
+	webmentions: {
+		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+	},
+};
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Uses",
+		path: "/uses/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
+
+const jsoncString = fs.readFileSync(new URL(`./subliminalnext.jsonc`, import.meta.url), "utf-8");
+const subliminalNextTheme = ExpressiveCodeTheme.fromJSONString(jsoncString);
+
+// https://expressive-code.com/reference/configuration/
+export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+	themes: [subliminalNextTheme],
+	themeCssSelector(theme, { styleVariants }) {
+		// If one dark and one light theme are available
+		// generate theme CSS selectors compatible with cactus-theme dark mode switch
+		if (styleVariants.length >= 2) {
+			const baseTheme = styleVariants[0]?.theme;
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
+			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
+		}
+		// return default selector
+		return `[data-theme="${theme.name}"]`;
+	},
+	useThemedScrollbars: false,
+	styleOverrides: {
+		frames: {
+			frameBoxShadowCssValue: "none",
+		},
+		uiLineHeight: "inherit",
+		codeFontSize: "0.875rem",
+		codeLineHeight: "1.7142857rem",
+		borderRadius: "4px",
+		codePaddingInline: "1rem",
+		codeFontFamily:
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+	},
+};
